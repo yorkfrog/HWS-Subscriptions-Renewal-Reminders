3d3a394368bf3aafdc7f5bfb439a8aaafeba55ac
Colin Froggatt <colin@froggatt.com>
2023-08-22 13:23:18 +0100

    HWS v1.2.2 updates

--- inc/base/renewal-reminders-send-notifications.php
+++ inc/base/renewal-reminders-send-notifications.php
@@ -33,44 +33,63 @@ class SPRRSendNotifications
         foreach($subscription_details as $subscription_detail ) {
             $subscription_id = $subscription_detail -> subscription__id;
             $subscription_details = wcs_get_subscription( $subscription_id );
-            $user_id = $subscription_details->get_user_id();
-            $user = get_user_by( 'id', $user_id );
-            if ($user && $options_en=='on') 
-            {
-                $subscriber_details['first_name'] = $user->first_name ;
-                $subscriber_details['last_name'] = $user->last_name;
-                $subscriber_details['email'] = $user->user_email;
-                $next_payment_date_dt = $subscription_details->get_date( 'next_payment' );
-                $next_payment_date = date( 'F d, Y', strtotime( $next_payment_date_dt ) );
-                $to = $subscriber_details['email'];
-                $subject = stripslashes_deep(esc_attr(get_option( 'email_subject' )));
-                $headers = array('Content-Type: text/html; charset=UTF-8'); 
-                // $headers = array('From:',$title); 
+                        
+            // @@@@@ HWS START
+			// period: day, week, month, year
+			$subscription_period =  $subscription_details->get_billing_period();
+			error_log("***** Subs ID / Period:" . $subscription_id . " / " . $subscription_period );
+			
+			$should_send_for_period = true;
+			$should_send_for_period = apply_filters( 'hws_renewal_reminders_should_send_for_period', $should_send_for_period, $subscription_period  );   	
+			if ($should_send_for_period) 
+			{
+            // @@@@@ HWS END
+            
+                $user_id = $subscription_details->get_user_id();
+                $user = get_user_by( 'id', $user_id );
+                if ($user && $options_en=='on') 
+                {
+                    $subscriber_details['first_name'] = $user->first_name ;
+                    $subscriber_details['last_name'] = $user->last_name;
+                    $subscriber_details['email'] = $user->user_email;
+                    $next_payment_date_dt = $subscription_details->get_date( 'next_payment' );
+                    $next_payment_date = date( 'F d, Y', strtotime( $next_payment_date_dt ) );
+                    $to = $subscriber_details['email'];
+                    $subject = stripslashes_deep(esc_attr(get_option( 'email_subject' )));
+                    $headers = array('Content-Type: text/html; charset=UTF-8'); 
+                    // $headers = array('From:',$title); 
+                    
+                    // @@@@@ HWS added $subscription_id param
+                    // $body = sprr_renewalremindersemail($subscriber_details['first_name'],$subscriber_details['last_name'],$next_payment_date);
+                    $body = sprr_renewalremindersemail($subscriber_details['first_name'],$subscriber_details['last_name'],$next_payment_date,$subscription_id);
+    
+                    // Function to change email address
+                    //code modified on version 1.1.2-for sprr_sender_email cannot redeclare error - #34571
+                    if (!function_exists('sprr_sender_email')) {
+                        function sprr_sender_email( $original_email_address ) {
+                        $admin_email = get_bloginfo('admin_email');
+                        return  $admin_email;
+                    }
+                    }
+                    
+                    // Function to change sender name
+                    if (!function_exists('sp_sender_name')) {
+                    function sp_sender_name( $original_email_from ) {
+                        $title = get_bloginfo();
+                        return $title;
+                    }
+                    }
+    
+                    // Hooking up our functions to WordPress filters 
+                    add_filter( 'wp_mail_from', 'sprr_sender_email' );
+                    add_filter( 'wp_mail_from_name', 'sp_sender_name' );
+                    wp_mail( $to, $subject, $body, $headers, array());  
+                }
                 
-                $body = sprr_renewalremindersemail($subscriber_details['first_name'],$subscriber_details['last_name'],$next_payment_date);
-
-                // Function to change email address
-                //code modified on version 1.1.2-for sprr_sender_email cannot redeclare error - #34571
-                if (!function_exists('sprr_sender_email')) {
-                    function sprr_sender_email( $original_email_address ) {
-                    $admin_email = get_bloginfo('admin_email');
-                    return  $admin_email;
-                }
-                }
-                
-                // Function to change sender name
-                if (!function_exists('sp_sender_name')) {
-                function sp_sender_name( $original_email_from ) {
-                    $title = get_bloginfo();
-                    return $title;
-                }
-                }
-
-                // Hooking up our functions to WordPress filters 
-                add_filter( 'wp_mail_from', 'sprr_sender_email' );
-                add_filter( 'wp_mail_from_name', 'sp_sender_name' );
-                wp_mail( $to, $subject, $body, $headers, array());  
-            }
+            // @@@@@ HWS START
+			} 			
+			// @@@@@ HWS END
+			
         }                          
     } // end send_subscriber_notification_emaill
 
